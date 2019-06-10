4de94a89ec2d7248d3dc04cc2ef2443f8564e988
ProSavage <naman0311@gmail.com>
2019-06-09 22:16:28 -0500

    Colored stuff will now work

--- src/main/java/net/prosavage/genbucket/menu/impl/GenerationShopGUI.java
+++ src/main/java/net/prosavage/genbucket/menu/impl/GenerationShopGUI.java
@@ -41,7 +41,7 @@ public class GenerationShopGUI extends MenuBuilder {
     private ItemStack getBlankItem() {
         // Auto converts to 1.8 / 1.13 material as needed.
         FileConfiguration config = getPlugin().getConfig();
-        ItemStack item = new ItemStack(MultiversionMaterials.fromString(config.getString("generation-shop.background-item")).parseMaterial());
+        ItemStack item = new ItemStack(MultiversionMaterials.fromString(config.getString("generation-shop.background-item")).parseItem().getType());
         if (!getPlugin().getServer().getVersion().contains("1.13")) {
             item.setDurability((short) config.getInt("generation-shop.background-item-durability"));
         }
