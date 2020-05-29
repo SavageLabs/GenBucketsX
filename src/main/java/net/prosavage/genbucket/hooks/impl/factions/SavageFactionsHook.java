12cb5bc4339fc3bbcbb0ff5b665fabf569878bd7
DRC-DEV <dracarys.pro@gmail.com>
2020-05-29 15:45:30 +0200

    Use reflection for canbuild check on fuuid forks

--- src/main/java/net/prosavage/genbucket/hooks/impl/factions/SavageFactionsHook.java
+++ src/main/java/net/prosavage/genbucket/hooks/impl/factions/SavageFactionsHook.java
@@ -4,15 +4,18 @@ import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
 import com.massivecraft.factions.listeners.FactionsBlockListener;
 import com.massivecraft.factions.struct.Relation;
+import com.massivecraft.factions.zcore.fperms.PermissableAction;
 import net.prosavage.genbucket.GenBucket;
 import net.prosavage.genbucket.hooks.impl.FactionHook;
 import net.prosavage.genbucket.utils.ChatUtils;
 import net.prosavage.genbucket.utils.Message;
 import net.prosavage.genbucket.utils.VanishUtils;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 public class SavageFactionsHook extends FactionHook {
@@ -22,9 +25,23 @@ public class SavageFactionsHook extends FactionHook {
         if (!GenBucket.get().getConfig().getBoolean("canbuild-check", true)) {
             return true;
         }
-        if (!FactionsBlockListener.playerCanBuildDestroyBlock(player, block.getLocation(), "build", true)) {
-            player.sendMessage(ChatUtils.color(Message.GEN_CANT_PLACE.getMessage()));
-            return false;
+        try {
+            if (!FactionsBlockListener.playerCanBuildDestroyBlock(player, block.getLocation(), "build", true)) {
+                player.sendMessage(ChatUtils.color(Message.GEN_CANT_PLACE.getMessage()));
+                return false;
+            }
+        } catch (NoSuchMethodError e) {
+            try {
+                Method canBuild = FactionsBlockListener.class.getMethod("playerCanBuildDestroyBlock", Player.class, Location.class, PermissableAction.class, boolean.class);
+                boolean canBuildCheck = (boolean) canBuild.invoke(FactionsBlockListener.class, player, block.getLocation(), PermissableAction.BUILD, true);
+                if (!canBuildCheck) {
+                    player.sendMessage(ChatUtils.color(Message.GEN_CANT_PLACE.getMessage()));
+                    return false;
+                }
+            } catch (Exception ex) {
+                ChatUtils.debug("Exception while checking for build permissions! Notify the author of the plugin.");
+                return false;
+            }
         }
         return true;
     }
